d31c9744330f1c6838e6e4b181683a34b3cf8462
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-11 11:15:38 +0600

    create Tasklist and Taskdetail class

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    return HttpResponse('Hlw')
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from . import models 
+
+class TaskList(ListView):
+    model = models.Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = models.Task 
+    context_object_name = 'tasks'
+    
     
