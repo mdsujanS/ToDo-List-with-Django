d670f64c548bff4a5e8fc5cd907207edc3d6bb35
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-12 01:22:35 +0600

    Create login & logout url

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .import views 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns=[
     path('', views.TaskList.as_view(), name='task-list'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-details'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', views.taskDelete.as_view(),name='task-delete'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
+    
     
     
 ]
