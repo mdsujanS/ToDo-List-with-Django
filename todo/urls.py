293da100839c8dfa116e3731d0b678def6c43692
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-11 11:18:07 +0600

    Create home and task urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views 
 urlpatterns=[
-    path('', views.home, name='home'),
+    path('', views.TaskList.as_view(), name='task-list'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-details'),
+    
 ]
