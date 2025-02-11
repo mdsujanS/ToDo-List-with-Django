3b883176662f68c3086e97fffc2684fa4341e2d2
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-11 21:56:42 +0600

    Add Create, Update & Delete Task url

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,9 @@ from .import views
 urlpatterns=[
     path('', views.TaskList.as_view(), name='task-list'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-details'),
+    path('task-create/', views.TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', views.taskDelete.as_view(),name='task-delete'),
+    
     
 ]
