0f173ed418de9a6dd78a9d583fdf1e7846c1f9c3
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-14 00:31:00 +0600

    add registation url

--- todo/urls.py
+++ todo/urls.py
@@ -10,6 +10,8 @@ urlpatterns=[
     path('task-delete/<int:pk>/', views.taskDelete.as_view(),name='task-delete'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.Registerpage.as_view(), name='register'),
+    
     
     
     
