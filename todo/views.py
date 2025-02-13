fb00226893cdec2890fcb36c425b647fe4d2cb14
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-02-14 00:35:22 +0600

    Create registation calss

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
-from django.urls import reverse_lazy 
-from . import models 
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth import login 
 from django.contrib.auth.mixins import LoginRequiredMixin 
 
+from django.urls import reverse_lazy 
+from . import models 
 
 class TaskList(LoginRequiredMixin, ListView):
     model = models.Task
@@ -16,7 +19,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
+        # context['count'] = context['tasks'].filter(complete=False).count()
         return context 
         
 
@@ -33,12 +36,6 @@ class TaskCreate(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
-# class TaskUpdate(UpdateView):
-#     models = models.Task 
-#     fields = '__all__'
-#     context_object_name = 'Tasks'
-#     success_url = reverse_lazy('task-list')
-#     template_name = 'todo/task_update.html'
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = models.Task
@@ -63,4 +60,23 @@ class CustomLogoutView(LogoutView):
     
     def get_success_url(self):
         return reverse_lazy('login')
+
+
+class Registerpage(FormView):
+    template_name = 'todo/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('login')
     
+    #save user in database
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Registerpage, self).form_valid(form)
+    
+    # if user login user don't go register page
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task-list')
+        return super(Registerpage, self).get(*args, **kwargs)
